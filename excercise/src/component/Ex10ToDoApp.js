6770c295b3bef9fc1ace34b029df204fcd2eaf5f
phapkhuong <phapkhuong@gmail.com>
2023-08-22 21:51:34 +0700

    To do App

--- excercise/src/component/Ex10ToDoApp.js
+++ excercise/src/component/Ex10ToDoApp.js
@@ -4,17 +4,47 @@ class Ex10ToDoApp extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            workList: []
+            workList: ["Hoc react", "Lam BPTC"],
+            workName: ""
         }
+    }
+
+    addNewWork() {
+        this.setState({
+            workList: [...this.state.workList, this.state.workName],
+            workName: ""
+        })
+    }
+
+    setWorkName(name) {
+        this.setState({
+            workName: name
+        })
     }
 
 
     render() {
         return (
             <>
-
-
-            </>)
+                <label>
+                    Nhap cong viec can lam
+                </label>
+                <input className={'form-control me-2'} value={this.state.workName}
+                       onChange={(event) => {
+                           this.setWorkName(event.target.value)
+                       }}/>
+                <button className={'btn btn-outline-success'} onClick={() => {
+                    this.addNewWork()
+                }}>Add new work
+                </button>
+                <h3>Danh sach cong viec can lam:</h3>
+                <ul>
+                    {this.state.workList.map((value, index) => {
+                        return <li key={index}>{value}</li>
+                    })}
+                </ul>
+            </>
+        )
     }
 }
 
